9509416dced766bc38e3920aac7b82c7b047a154
Fei Alm <fei.alm@telia.com>
2023-03-20 16:49:37 +0100

    Node in treemap clickable

--- js/treemap.js
+++ js/treemap.js
@@ -1,3 +1,32 @@
+/*var treeData ={children:
+[{name:"boss1",children:
+[
+{name:"mister_a",group:"A","value":28,colename:"level3"},
+{name:"mister_b",group:"A","value":19,colename:"level3"},
+{name:"mister_c",group:"C","value":18,colename:"level3"},
+{name:"mister_d",group:"C","value":19,colename:"level3"}
+]
+,colename:"level2"},
+{name:"boss2",children:
+[
+{name:"mister_e",group:"C","value":14,colename:"level3"},
+{name:"mister_f",group:"A","value":11,colename:"level3"},
+{name:"mister_g",group:"B","value":15,colename:"level3"},
+{name:"mister_h",group:"B","value":16,colename:"level3"}
+],
+colename:"level2"},
+{name:"boss3",children:
+[
+{name:"mister_i",group:"B","value":10,colename:"level3"},
+{name:"mister_j",group:"A","value":13,colename:"level3"},
+{name:"mister_k",group:"A","value":13,colename:"level3"},
+{name:"mister_l",group:"D","value":25,colename:"level3"},
+{name:"mister_m",group:"D","value":16,colename:"level3"},
+{name:"mister_n",group:"D","value":28,colename:"level3"}
+]
+,colename:"level2"}],name:"CEO"}*/
+
+
 var margin = { top: 20, right: 90, bottom: 20, left: 90 };
 var width = 960 - margin.left - margin.right;
 var height = 500 - margin.top - margin.bottom;
@@ -10,37 +39,41 @@ var svg = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("flare.json").then(function (data) {
+d3.json("data2.json").then(function (data) {
 
   console.log(data);
 
   const root = d3.hierarchy(data).sum(function(d){
-    return d.size;
+    return d.value;
   });
 
   var tree = d3.treemap().size([width, height])
     .padding(2)(root);
 
+
+  // prepare a color scale
   const color = d3.scaleOrdinal()
-    .domain([0, 1000])
-    .range(["#402D54", "#08306b"])
+    .domain(["boss1", "boss3", "boss3"])
+    .range([ "#402D54", "#D18975", "#8FD175"])
   
 
-  var nodeEnter = svg.selectAll("rect")
-    .data(tree.leaves())
+  // nu fungerarl ite saker här när man skriver function(event,d) istället
+  // kolla skillnaden på root.leaves() och tree.leaves()
+svg.selectAll("rect")
+    .data(root.leaves())
     .join("rect")
     .attr("id",function(d){return "node" + d.id})
     .attr("x", function(d) { return d.x0;})
     .attr("y", function(d) {return d.y0;})
     .attr("width", function(d) { return d.x1 - d.x0; })
     .attr("height", function(d) { return d.y1 - d.y0; })
-    .style("fill", "#08306b")
-    .on("click", function(d) {return console.log("CLICK", d.data.name); })
+    .style("fill", function(d){ return color(d.parent.data.name)})
+    .on("click", function(event,d) {return console.log("CLICK", d.data.name); })
   
   
-  nodeEnter
+svg
     .selectAll("text")
-    .data(tree.leaves())
+    .data(root.leaves())
     .join("text")
       .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
       .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
